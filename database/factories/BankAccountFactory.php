4a093ca4c8dab9e5ee3027288356e69b52068dd9
Maio Barbero <maiowebdesign@gmail.com>
2026-01-25 15:42:01 +0100

    Add bank accounts with model, migration, seeder and factory

---
+++ database/factories/BankAccountFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BankAccount>
+ */
+class BankAccountFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'name' => fake()->name(),
+            'balance' => fake()->numberBetween(1000, 100000),
+        ];
+    }
+}
---
+++ database/seeders/BankAccountSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BankAccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = \App\Models\User::all();
+        foreach ($users as $user) {
+            \App\Models\BankAccount::factory()->count(1)->create([
+                'user_id' => $user->id,
+            ]);
+        }
+    }
+}
