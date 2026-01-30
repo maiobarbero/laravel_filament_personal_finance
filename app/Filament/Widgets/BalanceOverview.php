aefc666747726e52efd5a56e632341815b94f6cf
Maio Barbero <maiowebdesign@gmail.com>
2026-01-30 18:17:35 +0100

    feat: prepend Euro currency symbol to displayed bank account balances

--- app/Filament/Widgets/BalanceOverview.php
+++ app/Filament/Widgets/BalanceOverview.php
@@ -2,19 +2,20 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\BankAccount;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use App\Models\BankAccount;
 
 class BalanceOverview extends StatsOverviewWidget
 {
     protected function getStats(): array
     {
-        $accounts = BankAccount::select("id", "name", "balance")->where("user_id", auth()->id())->get();
+        $accounts = BankAccount::select('id', 'name', 'balance')->where('user_id', auth()->id())->get();
         $stats = [];
         foreach ($accounts as $account) {
-            $stats[] = Stat::make($account->name, $account->balance);
+            $stats[] = Stat::make($account->name, "€ {$account->balance}");
         }
+
         return $stats;
     }
 }
