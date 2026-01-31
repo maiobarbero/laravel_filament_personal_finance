ac2cbe9be046b09611ff9f5b667abbb404266f5f
Maio Barbero <maiowebdesign@gmail.com>
2026-01-31 11:15:55 +0100

    Add total balance

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -5,6 +5,7 @@ namespace App\Filament\Widgets;
 use App\Models\BankAccount;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Collection;
 
 class StatsOverview extends StatsOverviewWidget
 {
@@ -12,10 +13,23 @@ class StatsOverview extends StatsOverviewWidget
     {
         $accounts = BankAccount::select('id', 'name', 'balance')->where('user_id', auth()->id())->get();
         $stats = [];
+
+        $stats[] = Stat::make('Total Balance', "€ {$this->calculateTotalBalance($accounts)}");
+
         foreach ($accounts as $account) {
             $stats[] = Stat::make($account->name, "€ {$account->balance}");
         }
 
         return $stats;
+    }
+
+    protected function calculateTotalBalance(Collection $accounts): float
+    {
+        $totalBalance = 0;
+        foreach ($accounts as $account) {
+            $totalBalance += $account->balance;
+        }
+
+        return $totalBalance;
     }
 }
