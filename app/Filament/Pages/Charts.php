d135d150477369aeee1299504fddcd6b76cdf38e
Maio Barbero <maiowebdesign@gmail.com>
2026-01-31 14:36:53 +0100

    Update page icon

--- app/Filament/Pages/Charts.php
+++ app/Filament/Pages/Charts.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use Filament\Support\Icons\Heroicon;
+use BackedEnum;
 
 class Charts extends Page
 {
     protected string $view = 'filament.pages.charts';
+    protected static string | BackedEnum | null $navigationIcon = Heroicon::ChartPie;
 }
